c02094920267e4ee1d9eca4fa3ea76d814fe9846
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-05-21 18:16:09 -0300

    Added comments

--- index.ino
+++ index.ino
@@ -15,6 +15,8 @@ int ultrassonicTrigger2 = 8;
 
 void setup() {
   Serial.begin(9600);
+
+  // Sets the green and red leds
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
 
@@ -22,12 +24,12 @@ void setup() {
   pinMode(ultrassonicTrigger1, OUTPUT);
   pinMode(ultrassonicEcho1, INPUT);
 
-  // Sets echo and trigger for the first Ultrassonic Sensor
+  // Sets echo and trigger for the second Ultrassonic Sensor
   pinMode(ultrassonicTrigger2, OUTPUT);
   pinMode(ultrassonicEcho2, INPUT);
 }
 
-void ultrassonicSensor(int trigger, int echo, int led, int minDistance, int delayBeforeActivation){
+void ultrassonicSensor(int trigger, int echo, int led, int minDistance, int delayBeforeActivation, int toiletNumber){
   digitalWrite(trigger, LOW);
   delayMicroseconds(2);
   digitalWrite(trigger, HIGH);
@@ -36,7 +38,7 @@ void ultrassonicSensor(int trigger, int echo, int led, int minDistance, int dela
 
   long duration = pulseIn(echo, HIGH);
   int distance = duration*0.034/2;
-  Serial.println(distance);
+  //Serial.println(distance);
 
   // Turns on led if sensor reads less than the minimum distance
   /*if(distance < minDistance){
@@ -47,22 +49,31 @@ void ultrassonicSensor(int trigger, int echo, int led, int minDistance, int dela
   }*/
 
   // If sensor reads less than the minimum distance, 
-  // triggers a simple time counter and after it goes
+  // it triggers a simple time counter and after it goes
   // off, start the action.
   if(distance < minDistance){
+    
+    char state[100];
+    sprintf(state, "Someone has got to the Toilet[%d], let's start counting:", toiletNumber);
+    Serial.println(state);
+    
     for(int i=0; i<delayBeforeActivation; i++){
+      Serial.println(i);
       digitalWrite(led, HIGH);
       delay(500);
       digitalWrite(led, LOW);
       delay(500);
     }
+  
+    Serial.println("Flushing!");
     digitalWrite(led, HIGH);
     delay(5000);
     digitalWrite(led, LOW);
+    Serial.println("Finished!\n");
   }
 }
 
 void loop() {
-  ultrassonicSensor(ultrassonicTrigger1, ultrassonicEcho1, greenLed, minDistance, delayBeforeActivation);
-  ultrassonicSensor(ultrassonicTrigger2, ultrassonicEcho2, redLed, minDistance, delayBeforeActivation);
+  ultrassonicSensor(ultrassonicTrigger1, ultrassonicEcho1, greenLed, minDistance, delayBeforeActivation, 1);
+  ultrassonicSensor(ultrassonicTrigger2, ultrassonicEcho2, redLed, minDistance, delayBeforeActivation, 2);
 }
